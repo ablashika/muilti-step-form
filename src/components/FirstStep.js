7b702f6cfc1c20206a9ecc36d29b6a03a9db749c
ablashika <kofoworola203@gmailcom>
2023-03-15 23:50:32 +0000

    first commit

---
+++ src/components/FirstStep.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function FirstStep() {
+  return (
+    <div className="Step-Conatiainer">
+        <div className='Step'></div>
+        <div className='StepCard'></div>
+
+    </div>
+  )
+}
