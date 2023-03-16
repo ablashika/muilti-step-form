45a7338bc1d42bd6cdeb96921e68ae8ab7173477
ablashika <kofoworola203@gmailcom>
2023-03-16 19:11:20 +0000

    added css

--- src/components/SecondStep.js
+++ src/components/SecondStep.js
@@ -1,1 +1,25 @@
-co
+import React from 'react'
+import  '../styles.css'
+
+export default function SecondStep() {
+  return (
+    <div>
+        <div className="StepConatiainer">
+        <div className='step'>
+        </div>
+        <div className='stepCard'>
+            <h1>SecondStep</h1>
+          
+            {/* <form>
+
+            </form> */}
+
+            <button>
+                Next
+            </button>
+        </div>
+
+    </div>
+    </div>
+  )
+}
