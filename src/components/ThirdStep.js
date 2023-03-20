2ccf7fc63fa604fd71cf26b4ef36eb2fddfbd15b
ablashika <kofoworola203@gmailcom>
2023-03-20 22:48:53 +0000

    added active step feature

---
+++ src/components/ThirdStep.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
+
+
+export default function ThirdStep({setActive}) {
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate('/step-four');
+        setActive('/step-four')
+      };
+  return (
+    
+    <div className='stepCard'>
+        <h1>SecondStep</h1>
+        <button 
+         onClick={handleClick}
+      
+         >
+            Next
+        </button>
+    </div>
+
+
+  )
+}
