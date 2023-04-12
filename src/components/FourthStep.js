e893243bee405a71a7a3f321e589287366fa0f20
ablashika <kofoworola203@gmailcom>
2023-04-12 19:42:05 +0000

    added few text content

--- src/components/FourthStep.js
+++ src/components/FourthStep.js
@@ -11,12 +11,21 @@ function FourthStep() {
       };
   return (
         <div className='stepCard'>
-            <h1>SecondStep</h1>
-            <button 
-            onClick={handleClick}
-            >
-                Next
-            </button>
+        <h1 >finishing Up</h1>
+        <h4 className='gray-text'>You have the option of monthly or yearly billing.</h4> 
+        
+        <div className='top-box'>
+       
+        
+        </div>  
+        <div className='nextStep-box'>
+        <button onClick={handleClick}
+        >
+            Next
+        </button>
+        </div>
+  
+            
         </div>
 
 
